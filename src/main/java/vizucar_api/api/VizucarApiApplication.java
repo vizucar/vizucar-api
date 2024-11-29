6d1ef8902310eba25590efc8757a268e684cfaaa
mohamed-rahmani <mohamed.rahmani@edu.univ-paris13.fr>
2024-11-29 11:03:03 +0100

    init springboot restful api project

---
+++ src/main/java/vizucar_api/api/VizucarApiApplication.java
@@ -0,0 +1,13 @@
+package vizucar_api.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VizucarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VizucarApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vizucar_api/api/VizucarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package vizucar_api.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VizucarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
